3f2f071de13fd89ca86cc84cb316ae7bf1e31a03
Christian Flor <christian.flor1@correo.icesi.edu.co>
2020-01-29 18:02:48 -0500

    Possible mockups added

---
+++ src/ui/Main.java
@@ -0,0 +1,43 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Icesi University (Cali - Colombia)
+ * @author Natalia Isabel Gonzalez Murillo <natalia.gonzalez3@correo.icesi.edu.co>
+ * @author Christian David Flor Astudillo <christian.flor1@correo.icesi.edu.co>
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+package ui;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * This class 
+ */
+public class Main extends Application{
+	
+	/**
+	 * This class represents
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch();
+	}
+	
+	/**
+	 * This class represents
+	 */
+	@Override
+	public void start(Stage stage) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("gui.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		stage.setTitle("");
+		stage.setScene(scene);
+		stage.show();
+	}
+
+}
